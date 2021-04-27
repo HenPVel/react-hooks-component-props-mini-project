34ab1a90adf11d58071cad36821d499dd9b4133b
Henry <hvelas93@gmail.com>
2021-04-27 19:32:14 -0400

    finished mini project without bonus

---
+++ src/components/ArticleList.js
@@ -0,0 +1,18 @@
+import Article from "./Article"
+import blogData from "../data/blog"
+
+let articleElements = blogData.posts.map((post,index) => {
+    return(
+        <Article {...post}/>
+    )
+})
+
+function ArticleList() {
+    return (
+        <main>
+        {articleElements}
+        </main>
+    )
+}
+
+export default ArticleList
