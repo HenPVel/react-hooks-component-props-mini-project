21a15ea9aec8f53ee1dd2ad44942679b6a0bd599
Henry <hvelas93@gmail.com>
2021-04-27 19:55:01 -0400

    props mini project with bonus

--- src/components/Article.js
+++ src/components/Article.js
@@ -1,11 +1,23 @@
 
 
-function Article({title, date = "January 1, 1970", preview}) {
-    console.log(title, date, preview)
+function Article({title, date = "January 1, 1970", preview, minutes}) {
+    let emojiTally
+    let minuteCount5
+    let minuteCount10
+    
+    minuteCount5 = Math.round(minutes/5)
+    minuteCount10 = Math.round(minutes/10)
+    
+    if (minutes < 30) {emojiTally = "☕️".repeat(minuteCount5)}
+    if (minutes > 30) {emojiTally = "🍱".repeat(minuteCount10)}
+
+    
+
     return (
         <article>
         <h3>{title}</h3>
         <small>{date}</small>
+        <small> - {emojiTally} {minutes} min read</small>
         <p>{preview}</p>
         </article>
     )
